23f22b353ff33e8b6675c698840e21e90e7c544e
Dominik Werder <dominik.werder@psi.ch>
2018-04-10 14:24:07 +0200

    Merge logger functionality

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,18 +1,11 @@
 #include "logger.h"
 #include "KafkaW/KafkaW.h"
 #include <atomic>
+#include <atomic>
 #include <cstdarg>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#ifdef _MSC_VER
-#include <io.h>
-#include <iso646.h>
-#define isatty _isatty
-#else
-#include <unistd.h>
-#endif
-#include <atomic>
 #include <memory>
 #include <nlohmann/json.hpp>
 #include <string>
@@ -37,9 +30,8 @@ public:
   void log_kafka_gelf_start(std::string broker, std::string topic);
   void log_kafka_gelf_stop();
   FILE *log_file = stdout;
-  int is_tty = 1;
-  void dwlog_inner(int level, int color, char const *file, int line,
-                   char const *func, std::string const &s1);
+  void dwlog_inner(int level, char const *file, int line, char const *func,
+                   std::string const &s1);
   int prefix_len();
   void fwd_graylog_logger_enable(std::string address);
 
@@ -51,7 +43,7 @@ private:
   std::thread thread_poll;
 };
 
-Logger::Logger() { is_tty = isatty(fileno(log_file)); }
+Logger::Logger() {}
 
 Logger::~Logger() {
   do_run_kafka = false;
@@ -67,7 +59,6 @@ Logger::~Logger() {
 void Logger::use_log_file(std::string fname) {
   FILE *f1 = fopen(fname.c_str(), "wb");
   log_file = f1;
-  is_tty = isatty(fileno(log_file));
 }
 
 void Logger::log_kafka_gelf_start(std::string Address, std::string topicname) {
@@ -93,7 +84,6 @@ void Logger::log_kafka_gelf_stop() {
 }
 
 void Logger::fwd_graylog_logger_enable(std::string address) {
-#ifdef HAVE_GRAYLOG_LOGGER
   auto addr = address;
   int port = 12201;
   auto col = address.find(":");
@@ -101,16 +91,20 @@ void Logger::fwd_graylog_logger_enable(std::string address) {
     addr = address.substr(0, col);
     port = strtol(address.c_str() + col + 1, nullptr, 10);
   }
+#ifdef HAVE_GRAYLOG_LOGGER
   Log::RemoveAllHandlers();
-  LOG(4, "Enable graylog_logger on {}:{}", addr, port);
+  LOG(Sev::Info, "Enable graylog_logger on {}:{}", addr, port);
   Log::AddLogHandler(new GraylogInterface(addr, port));
   do_use_graylog_logger = true;
 #else
-  LOG(0, "ERROR not compiled with support for graylog_logger");
+  LOG(Sev::Notice,
+      "ERROR not compiled with support for graylog_logger. Would have used "
+      "{}:{}",
+      addr, port);
 #endif
 }
 
-void Logger::dwlog_inner(int level, int color, char const *file, int line,
+void Logger::dwlog_inner(int level, char const *file, int line,
                          char const *func, std::string const &s1) {
   int npre = prefix_len();
   int const n2 = strlen(file);
@@ -119,30 +113,9 @@ void Logger::dwlog_inner(int level, int color, char const *file, int line,
     npre = 0;
   }
   auto f1 = file + npre;
-  {
-    // only use color for stdout
-    std::string lmsg;
-    if (is_tty && color > 0 && color < 8) {
-      static char const *cols[]{
-          "",
-          "\x1b[107;1;31m",
-          "\x1b[100;1;33m",
-          "\x1b[107;1;35m",
-          "\x1b[107;1;36m",
-          "\x1b[107;1;34m",
-          "\x1b[107;1;32m",
-          "\x1b[107;1;30m",
-      };
-      lmsg = fmt::format("{}:{} [{}]:  {}{}\x1b[0m\n", f1, line, level,
-                         cols[color], s1);
-    } else {
-      lmsg = fmt::format("{}:{} [{}]:  {}\n", f1, line, level, s1);
-    }
-    fwrite(lmsg.c_str(), 1, lmsg.size(), log_file);
-  }
+  auto lmsg = fmt::format("{}:{} [{}]:  {}\n", f1, line, level, s1);
+  fwrite(lmsg.c_str(), 1, lmsg.size(), log_file);
   if (level < 7 && do_run_kafka.load()) {
-    // Format again without color
-    auto lmsg = fmt::format("{}:{} [{}]:  {}\n", f1, line, level, s1);
     // If we will use logging to Kafka in the future, refactor a bit to reduce
     // duplicate work..
     using nlohmann::json;
@@ -158,8 +131,6 @@ void Logger::dwlog_inner(int level, int color, char const *file, int line,
   }
 #ifdef HAVE_GRAYLOG_LOGGER
   if (do_use_graylog_logger.load() and level < 7) {
-    // Format again without color
-    auto lmsg = fmt::format("{}:{} [{}]:  {}\n", f1, line, level, s1);
     Log::Msg(level, lmsg);
   }
 #endif
@@ -172,13 +143,14 @@ int Logger::prefix_len() {
 }
 
 static Logger g__logger;
-}
+
+} // namespace DW
 
 void use_log_file(std::string fname) { DW::g__logger.use_log_file(fname); }
 
-void dwlog_inner(int level, int c, char const *file, int line, char const *func,
+void dwlog_inner(int level, char const *file, int line, char const *func,
                  std::string const &s1) {
-  DW::g__logger.dwlog_inner(level, c, file, line, func, s1);
+  DW::g__logger.dwlog_inner(level, file, line, func, s1);
 }
 
 void log_kafka_gelf_start(std::string broker, std::string topic) {
